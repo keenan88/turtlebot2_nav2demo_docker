5103c568de1f0bfed2baccdec8f6e4678042957a
Keenan88 <Lakingsfan08@gmail.com>
2022-10-28 15:15:12 -0400

    stable. both robots spawn and have path planning

--- docker_bootstrap/Scripts/tb3_nav2demo_container-start.sh
+++ docker_bootstrap/Scripts/tb3_nav2demo_container-start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-gnome-terminal -- bash -c "source install/setup.bash; ros2 launch turtlebot3_gazebo turtlebot3_tc_world_two_robots.launch.py; python3; exec bash"
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch turtlebot3_gazebo turtlebot3_tc_world_two_robots.launch.py; exec bash"
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch path_planner_server multi_navigation.launch.py; exec bash"
 
 
 bash # Prevents container from closing
